fc7a8accc534fcffee7c1a5f4d5138dd9d8b7606
Len <lentiancn@126.com>
2026-05-22 16:22:09 +0800

    Add new file

--- skill-alpine/scripts/alpine_latest_version_archs_aggregation.sh
+++ skill-alpine/scripts/alpine_latest_version_archs_aggregation.sh
@@ -9,11 +9,11 @@ set -euo pipefail
 latest_releases_json=$(bash ./alpine_fetch_latest_releases.sh)
 
 echo "$latest_releases_json" | jq -c '
-  reduce .[] as $item ({};
-    .[$item.version] += [$item.arch]
-  )
-  | to_entries | sort_by(.key) | map({
-    version: .key,
-    archs: (.value | sort)
-  })
-'
+    reduce .[] as $item ({};
+      .[$item.version] += [$item.arch]
+    )
+    | to_entries | sort_by(.key) | map({
+      version: .key,
+      archs: (.value | sort)
+    })
+  '
